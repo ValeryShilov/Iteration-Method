b1ee4a36272eb6238042c5d26a0a28e0e46f7d77
ValeryShilov <valera.shilov.03@mail.ru>
2022-12-21 20:18:04 +0500

    1

--- Метод итераций.cpp
+++ Метод итераций.cpp
@@ -2,17 +2,19 @@
 #include <cmath>
 using namespace std;
 double f(double n) {
-    return pow(n, 3) + 47 * n - 15;  //уравнение
+    //return pow(n, 3) + 47 * n - 15;  //уравнение
+    return (15 - pow(n, 3)) / 47;
 }
 double fp(double n) {
     return 3 * pow(n, 2) + 47;      //производная
 }
 int main() {
     setlocale(LC_ALL, "RUS");
-    double r = 1, a, b, x0, e;
+    double r = 1, a, b, x0, e = pow(10,-6);
     bool flag = 1;
-
-    cin >> a >> b >> e >> x0;
+    int cnt = 0;
+    
+    cin >> a >> b >> x0;
 
     do {
         double x1 = f(x0);
@@ -21,8 +23,12 @@ int main() {
         if (abs(f(x1)) >= 1) {
             flag = 0;
         }
+        cnt++;
     } while (r > e && flag);
     if (flag == 0) cout << "На заданном отрезке корней нет" << endl;
-    cout << x0 << endl;
+    else {
+        cout << "x = " << x0 << endl;
+        cout << "Количество итераций = " << cnt << endl;
+    }
     return 0;
 }
